fa8756e1f126bbb5177163d6a6e59098d72f1e7b
ANgajasinghe <Akalanka Nayanajith@DESKTOP-TERE1H0>
2021-02-14 20:08:02 +0530

    One-Item

--- hooks/src/components/Todo.js
+++ hooks/src/components/Todo.js
@@ -1,33 +1,44 @@
 // useState is a Hook 
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 
 
 const Todo = prop => {
 
     // this will take initial state
+
     // useState -02
     const [todoName, setTodoName] = useState('');
-
     // useState -03
     const [todoList, setTodoList] = useState([]);
 
-    const todoAddHanddler = () => {
-        setTodoList(todoList.concat(todoName));
-        console.log(todoList);
-    }
+
+
+    const [todoState, setTodoState] = useState({ userInput: '', todoList: [] })
+
 
     const inputChangeHandler = (event) => {
-        setTodoName(event.target.value);
+        setTodoState({ 
+            userInput: event.target.value,
+            todoList: todoState.todoList 
+            });
         console.log(todoName);
     }
 
-        // Like Aux allow us to return top level  
+    const todoAddHanddler = () => {
+        setTodoState({ 
+            userInput: todoState.userInput,
+            todoList: todoState.todoList.concat(todoState.userInput) 
+            });
+        console.log(todoList);
+    }
+
+    // Like Aux allow us to return top level  
     return <React.Fragment>
-        <input type="text" placeholder="Todo" onChange={inputChangeHandler}  value={todoName}/>
+        <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoState.userInput} />
         <button type="button" onClick={todoAddHanddler}> Add </button>
 
         <ul>
-            {todoList.map(item=>
+            {todoState.todoList.map(item =>
             (
                 <li key={item}>{item}</li>
             ))}
